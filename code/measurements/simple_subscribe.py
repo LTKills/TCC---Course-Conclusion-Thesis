38c22a0bc3a3a88684617fd2619e845453cefbb1
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-05-21 12:15:56 -0300

    Change dir strucutre

---
+++ code/measurements/simple_subscribe.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import time
+
+
+"""
+callback function 'on_message'
+called when client receives a message
+"""
+
+def on_message(client, userdata, message):
+    print(message)
+
+
+host = 'localhost'
+port = 1883
+
+client = mqtt.Client('simple_sub')
+client.on_message = on_message
+
+client.connect(host, port)
+client.subscribe('ping_t')
+
+client.loop_start()
+time.sleep(100)
+client.loop_stop()
